7a050c5069e444d918c5679b36423011e623ece6
Alex Catalan <alexcatmu@gmail.com>
2020-12-12 17:18:18 +0100

    Threads.jsx bug

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -2,7 +2,6 @@ export const UPVOTED_SUBMISSIONS = "UPVOTED_SUBMISSIONS";
 export const UPVOTED_COMMENTS = "UPVOTED_COMMENTS";
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
-export const COMMENTS = "COMMENTS";
 export const THREADS = "THREADS";
 export const SUBMISSIONS = "SUBMISSIONS";
 export const USER = "USER";
